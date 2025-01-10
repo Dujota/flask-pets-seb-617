06f8d8bddaa4e5c45d1916ef0c00b2fc1b00eaa4
Denis S Dujota <denisdujota@gmail.com>
2025-01-10 04:56:51 +0300

    finish flask pets

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 # Import the 'Flask' class from the 'flask' library.
 import psycopg2, psycopg2.extras
 from flask import Flask, request
+from flask_cors import CORS
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -12,6 +13,7 @@ import os
 # Initialize Flask
 # We'll use the pre-defined global '__name__' variable to tell Flask where it is.
 app = Flask(__name__)
+CORS(app)
 
 def get_db_connection():
     connection = psycopg2.connect(
@@ -66,6 +68,36 @@ def show_pet(pet_id):
         return pet, 200
     except Exception as e:
         return str(e), 500
+
+@app.route('/pets/<pet_id>', methods=['DELETE'])
+def delete_pet(pet_id):
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cursor.execute("DELETE FROM pets WHERE id = %s", (pet_id,))
+        if cursor.rowcount == 0:
+            return "Pet not found", 404
+        connection.commit()
+        cursor.close()
+        return "Pet deleted successfully", 204
+    except Exception as e:
+        return str(e), 500
+
+@app.route('/pets/<pet_id>', methods=['PUT'])
+def update_pet(pet_id):
+    try:
+      pet = request.json
+      connection = get_db_connection()
+      cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+      cursor.execute("UPDATE pets SET name = %s, age = %s, breed = %s WHERE id = %s RETURNING *", (pet['name'], pet['age'], pet['breed'], pet_id))
+      updated_pet = cursor.fetchone()
+      if updated_pet is None:
+        return "Pet Not Found", 404
+      connection.commit()
+      connection.close()
+      return updated_pet, 202
+    except Exception as e:
+      return str(e), 500
 
 # Run our application, by default on port 5000
 app.run()
